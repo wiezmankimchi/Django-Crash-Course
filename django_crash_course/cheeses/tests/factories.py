6a3ebcbaeae7ad787a6e0bfb663808fd98e53b9b
Wiezman Kimchi <wiezmankimchi@gmail.com>
2020-05-23 11:55:20 -0700

    add bash scripts

--- django_crash_course/cheeses/tests/factories.py
+++ django_crash_course/cheeses/tests/factories.py
@@ -4,6 +4,7 @@ import factory
 import factory.fuzzy
 
 from ..models import Cheese
+from django_crash_course.users.tests.factories import UserFactory
 
 class CheeseFactory(factory.django.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText()
@@ -11,6 +12,7 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     description = factory.Faker('paragraph', nb_sentences=3, variable_nb_sentences=3)
     firmness = factory.fuzzy.FuzzyChoice( [x[0] for x in Cheese.Firmness.choices])
     country_of_origin = factory.Faker('country_code')
+    creator = factory.SubFactory(UserFactory)
 
     class Meta:
         model=Cheese
